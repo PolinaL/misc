930080924254ed26a4f4c2fedd871878839a25e5
polinal <plltvk@gmail.com>
2017-04-03 13:22:06 +0200

    removed path

--- download_from_server.py
+++ download_from_server.py
@@ -3,7 +3,6 @@ import urllib.request
 import pycurl
 
 url = "http://littleheron.iwr.uni-heidelberg.de:8081/"
-path = "/export/home/fdiego/vsu/cremi"
 pattern = '(?<=a href=").*py(?=")'
 response = urllib.request.urlopen(url).read()
 
@@ -12,7 +11,7 @@ for filename in re.findall(pattern, response.decode("utf-8")):
     print(filename)
     fp = open(filename, "wb")
     curl = pycurl.Curl()
-    curl.setopt(pycurl.URL, url+path+filename)
+    curl.setopt(pycurl.URL, url+filename)
     curl.setopt(pycurl.WRITEDATA, fp)
     curl.perform()
     curl.close()
